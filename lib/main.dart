039c0d63d3dad40baf0b73df42d6ba2d683e44e3
Indrajit <indrajit30623@gmail.com>
2024-02-21 22:34:57 +0530

    creating a quize app

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart' ;
+
+
+void main (){
+  return runApp(const MaterialApp(
+    home: Scaffold(body: Text('hello'),),
+  ));
+}
---
+++ lib/startScreen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class StartScreen extends StatelessWidget {
+
+  StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
